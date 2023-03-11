dfeb7d8833da6cc9220cf2d696b0bef7bdf57f70
MoeMoeee <dangkhoa.140502@gmail.com>
2023-03-11 12:32:18 +1100

    Remove deleted files (backtesting.ipynb and hehe.py) and update test.py

--- test.py
+++ test.py
@@ -1,60 +1,61 @@
-from data_model import Listing, OrderDepth, Trade, TradingState
+from algo_test import Listing, OrderDepth, Trade, TradingState
 
 timestamp = 1000
 
 listings = {
-	"PRODUCT1": Listing(
-		symbol="PRODUCT1", 
-		product="PRODUCT1", 
-		denomination: "SEASHELLS"
-	),
-	"PRODUCT2": Listing(
-		symbol="PRODUCT2", 
-		product="PRODUCT2", 
-		denomination: "SEASHELLS"
-	),
+    "PRODUCT1": Listing(
+        symbol="PRODUCT1",
+        product="PRODUCT1",
+        denomination: "SEASHELLS"
+    ),
+
+    "PRODUCT2": Listing(
+        symbol="PRODUCT2",
+        product="PRODUCT2",
+        denomination: "SEASHELLS"
+    ),
 }
 
 order_depths = {
-	"PRODUCT1": OrderDepth(
-		buy_orders={10: 7, 9: 5},
-		sell_orders={11: -4, 12: -8}
-	),
-	"PRODUCT2": OrderDepth(
-		buy_orders={142: 3, 141: 5},
-		sell_orders={144: -5, 145: -8}
-	),	
+    "PRODUCT1": OrderDepth(
+        buy_orders={10: 7, 9: 5},
+        sell_orders={11: -4, 12: -8}
+    ),
+    "PRODUCT2": OrderDepth(
+        buy_orders={142: 3, 141: 5},
+        sell_orders={144: -5, 145: -8}
+    ),
 }
 
 own_trades = {
-	"PRODUCT1": [],
-	"PRODUCT2": []
+    "PRODUCT1": [],
+    "PRODUCT2": []
 }
 
 market_trades = {
-	"PRODUCT1": [
-		Trade(
-			symbol="PRODUCT1",
-			price=11,
-			quantity=4,
-			buyer="",
-			seller="",
-		)
-	],
-	"PRODUCT2": []
+    "PRODUCT1": [
+        Trade(
+            symbol="PRODUCT1",
+            price=11,
+            quantity=4,
+            buyer="",
+            seller="",
+        )
+    ],
+    "PRODUCT2": []
 }
 
 position = {
-	"PRODUCT1": 3,
-	"PRODUCT2": -5
+    "PRODUCT1": 3,
+    "PRODUCT2": -5
 }
 
 observations = {}
 
 state = TradingState(
-	timestamp=timestamp,
-  listings=listings,
-	order_depths=order_depths,
-	own_trades,
-	market_trades
+    timestamp=timestamp,
+    listings=listings,
+    order_depths=order_depths,
+    own_trades,
+    market_trades
 )
