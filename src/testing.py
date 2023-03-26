f377be38c1ed788cd293029f89731ea2f8e4aac3
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-03-27 01:02:22 +1100

    updated new test file :)

--- src/testing.py
+++ src/testing.py
@@ -107,29 +107,116 @@ pina = "PINA_COLADAS"
 berrie = "BERRIES"
 diving = "DIVING_GEAR"
 
-# while checktime <= 1:
 
-bananas_df = df[df['product'] == bananas]
-row = bananas_df.iloc[0]
-bid_price_1 = row['bid_price_1']
-bid_volume_1 = row['bid_volume_1']
-bid_price_2 = row['bid_price_2']
-bid_volume_2 = row['bid_volume_2']
-bid_price_3 = row['bid_price_3']
-bid_volume_3 = row['bid_volume_3']
-ask_price_1 = row['ask_price_1']
-ask_volume_1 = row['ask_volume_1']
-ask_price_2 = row['ask_price_2']
-ask_volume_2 = row['ask_volume_2']
-ask_price_3 = row['ask_price_3']
-ask_volume_3 = row['ask_volume_3']
+def assign(row, product):
+    dataframe = df[df['product'] == product]
+    row = dataframe.iloc[row]
+    bid_price_1 = row['bid_price_1']
+    bid_volume_1 = row['bid_volume_1']
+    bid_price_2 = row['bid_price_2']
+    bid_volume_2 = row['bid_volume_2']
+    bid_price_3 = row['bid_price_3']
+    bid_volume_3 = row['bid_volume_3']
+    ask_price_1 = row['ask_price_1']
+    ask_volume_1 = row['ask_volume_1']
+    ask_price_2 = row['ask_price_2']
+    ask_volume_2 = row['ask_volume_2']
+    ask_price_3 = row['ask_price_3']
+    ask_volume_3 = row['ask_volume_3']
 
-order_depths[bananas] = OrderDepth(
-    buy_orders={bid_price_1: bid_volume_1,
-                bid_price_2: bid_volume_2,
-                bid_price_3: bid_volume_3},
-    sell_orders={ask_price_1: -ask_volume_1,
-                 ask_price_2: -ask_volume_2,
-                 ask_price_3: -ask_volume_3}
-)
-print(order_depths[bananas].buy_orders)
+    order_depths[product] = OrderDepth(
+        buy_orders={bid_price_1: bid_volume_1,
+                    bid_price_2: bid_volume_2,
+                    bid_price_3: bid_volume_3},
+        sell_orders={ask_price_1: -ask_volume_1,
+                     ask_price_2: -ask_volume_2,
+                     ask_price_3: -ask_volume_3}
+    )
+    # print(order_depths[product].buy_orders)
+
+
+print(assign(1, bananas))
+while checktime <= 10:
+    assign(index, coconnuts)
+    assign(index, bananas)
+    assign(index, pina)
+    assign(index, berrie)
+
+    result = trader.run(state=TradingState(
+        timestamp=checktime,
+        listings=listings,
+        order_depths=order_depths,
+        own_trades=own_trades,
+        market_trades=market_trades,
+        position=position_quant,
+        observations=observations
+    ))
+
+    for item in profit:
+        if result[item]:
+            quantity_temp = result[item][0].quantity
+            if np.sign(quantity_temp) == 1:
+                if quantity_temp > -list(order_depths[item].sell_orders.values())[0]:
+                    quantity = - \
+                        list(order_depths[item].sell_orders.values())[0]
+                else:
+                    quantity = quantity_temp
+            elif np.sign(quantity_temp) == -1:
+                if quantity_temp < -list(order_depths[item].buy_orders.values())[0]:
+                    quantity = -list(order_depths[item].buy_orders.values())[0]
+                else:
+                    quantity = quantity_temp
+            else:
+                quantity = 0
+
+            price = result[item][0].price
+
+            # profit = profit[item]
+
+            if np.sign(quantity) == np.sign(position_quant[item]) or position_quant[item] == 0 or quantity == 0:
+                # calculate average
+                position_average[item] = (
+                    position_quant[item]*position_average[item] + quantity*price) / (quantity + position_quant[item])
+                position_quant[item] += quantity
+
+            else:
+                # Trade is smaller
+                if abs(quantity) < abs(position_quant[item]):
+                    # Close part of Long position
+                    if position_quant[item] > 0:
+                        profit[item] += abs(quantity)*price - \
+                            position_average[item]*abs(quantity)
+                        position_quant[item] += quantity
+                        # Position avg price stay same
+
+                    # Close part of Short position
+                    else:
+                        profit[item] += position_average[item] * \
+                            abs(quantity) - abs(quantity)*price
+                        position_quant[item] += quantity
+                        # Position avg price stay same
+
+                # Trade is larger
+                else:
+                    # Close all of Long position and turn to Short
+                    if position_quant[item] > 0:
+
+                        profit[item] += abs(position_quant[item])*price - \
+                            position_average[item]*abs(position_quant[item])
+                        position_quant[item] += quantity
+                        position_average[item] = price
+
+                    # Close part of Short position and turn to Long
+                    else:
+                        profit[item] += position_average[item] * \
+                            abs(position_quant[item]) - \
+                            abs(position_quant[item])*price
+                        position_quant[item] += quantity
+                        position_average[item] = price
+
+    # put the result in to output.csv file
+    with open("output.csv", "a") as f:
+        print(checktime, result, profit, file=f)
+
+    checktime += 100
+    index += 1
